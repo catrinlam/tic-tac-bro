6ceaefb438f7d6033296dcb68d1014ccb0bf197a
Amina <154568053+aminoacid-01@users.noreply.github.com>
2024-12-12 11:33:23 +0000

    create ticTac object

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
+const ticTac = {
+    currentPlayer: "X",
+    state: ["", "", "", "", "", "", "", "", ""],
+    gameOver: false,
+
+    init: function(){
+        this.createBoard();
+        document.getElementById("reset").addEventListener("click", () => this.resetGame());
+    }
+}
 
 // create the board
-function createBoard(){}
+function createBoard(){
+createBoard: function(){
+    board.innerhtml = '';
+    this.state.forEach((_, i) => {
+        const cell = document.createElement("div");
+        cell.setAttribute(
+            "style",
+            "width: 100px; height: 100px; border: 1px solid black; display: flex; jusify-content: center; font-size: 3em; cursor: pointer;"
+        );
+        cell.dataset.index = i;
+        board.appendChild(cell);
+    });
+    board.addEventListener("click", (e) => this.handleClick(e)); // handle clicks on board
+    this.uMessage(`Player ${this.currentPlayer}'s turn`);
+}
+
 
 
 
 
 // handle cicks on cells
 function handleClick(e){}
-
+handleClick: function(e){}
 // check for a win
 function checkForWin(){}
-
+checkForWin: function(){}
 
 // highlight the winning cells
 function highlight(){
@@ -19,3 +44,5 @@ function highlight(){
 // reset the game
 function resetGame(){
 }
+
+ticTac.init();
