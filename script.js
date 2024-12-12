6a43ee6dd092e8de3ebd58f88fb27bb085ab98b2
Amina <154568053+aminoacid-01@users.noreply.github.com>
2024-12-12 11:09:57 +0000

    add basic js layout

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// create the board
+function createBoard(){}
+
+
+
+
+// handle cicks on cells
+function handleClick(e){}
+
+// check for a win
+function checkForWin(){}
+
+
+// highlight the winning cells
+function highlight(){
+}
+
+// reset the game
+function resetGame(){
+}
