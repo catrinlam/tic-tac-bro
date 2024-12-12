19166753f73de951ed1c13aaaf577d9a7472bd55
Amina <154568053+aminoacid-01@users.noreply.github.com>
2024-12-12 11:43:25 +0000

    add win combos

--- script.js
+++ script.js
@@ -17,7 +17,21 @@ const ticTac = {
 function handleClick(e){}
 
 // check for a win
-function checkForWin(){}
+function checkForWin(){
+    const wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8], // Rows
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8], // Columns
+        [0, 4, 8],
+        [2, 4, 6], // Diagonals
+    ];
+    return wins.find((combo) =>
+        combo.every((i) => this.state[i] === this.currentPlayer)
+    );
+},
 
 
 // highlight the winning cells
