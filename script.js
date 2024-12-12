e9d519960d35a27cc80b0d5db2fec535151c0a70
Amina <154568053+aminoacid-01@users.noreply.github.com>
2024-12-12 11:35:29 +0000

    remove createBoard function

--- script.js
+++ script.js
@@ -3,28 +3,11 @@ const ticTac = {
     state: ["", "", "", "", "", "", "", "", ""],
     gameOver: false,
 
-    init: function(){
-        this.createBoard();
-        document.getElementById("reset").addEventListener("click", () => this.resetGame());
-    }
-}
+    
+};
 
 // create the board
-function createBoard(){
-createBoard: function(){
-    board.innerhtml = '';
-    this.state.forEach((_, i) => {
-        const cell = document.createElement("div");
-        cell.setAttribute(
-            "style",
-            "width: 100px; height: 100px; border: 1px solid black; display: flex; jusify-content: center; font-size: 3em; cursor: pointer;"
-        );
-        cell.dataset.index = i;
-        board.appendChild(cell);
-    });
-    board.addEventListener("click", (e) => this.handleClick(e)); // handle clicks on board
-    this.uMessage(`Player ${this.currentPlayer}'s turn`);
-}
+
 
 
 
@@ -32,10 +15,10 @@ createBoard: function(){
 
 // handle cicks on cells
 function handleClick(e){}
-handleClick: function(e){}
+
 // check for a win
 function checkForWin(){}
-checkForWin: function(){}
+
 
 // highlight the winning cells
 function highlight(){
